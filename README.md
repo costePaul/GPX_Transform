4e9755929c407921eef4e7d9f5669a6c125e817d
Paul Coste <paul@lescoste.net>
2022-03-31 21:39:38 +0200

    changs the name of the gpx

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Removes some layers on GPX file so that multi-stages gpx (from https://www.viarhona.com or https://www.francevelotourisme.com for example) can be properly visualised on GPX Viewer.
+Removes some layers on GPX file so that multi-stages gpx (from https://www.viarhona.com or https://www.francevelotourisme.com for example) can be fully visualised on GPX Viewer.
 
-Use : ```python3 gpx_transform <original_gpx_path> <transformed_gpx_output_path>```
+Use : ```python3 gpx_transform <original_gpx_file_path> <new_gpx_name>```
+
+The output file will be named 'same-path/output-same-name.gpx' and the ```<name>``` inside the file will be set to '<new_gpx_name>' so it is named 'new_gpx_name' on GPX Viewer.
--- gpx_transform.py
+++ gpx_transform.py
@@ -6,28 +6,56 @@ def condition_line(line, trigger):
     bool = (line != '</gpx>'  and (trigger == 'pass' or line == trigger))
     return bool
 
+def get_image_directory_and_name(path):
+    last_slash_index = 0
+    last_dot_index = 0
+    n = len(path)
+    for i in range(n):
+        if path[i]=='/':
+            last_slash_index = i
+    for i in range(last_slash_index,n):
+        if path[i]=='.':
+            last_dot_index = i
+    if last_dot_index==0:
+        return path[:last_slash_index],path
+    else :
+        return path[:last_slash_index],path[last_slash_index+1:last_dot_index]
+
 def main():
     input_filename = sys.argv[1]
-    output_filename = sys.argv[2]
+    new_filename = sys.argv[2]
+    dir,name = get_image_directory_and_name(input_filename)
+    result_path = dir+'/output-'+name+'.gpx'
+    output_filename = result_path
+
+    name_has_changed = False
+
     with open(input_filename, "r") as f:
         lines = f.read().split("\n")
     output = []
     offset = 0
 
     for i,line in enumerate(lines):
-        if offset:
-            offset -=1
-        else:
-            line = lines[i].lstrip()
-            j = 0
-            cond = True
-            while j<5 and cond:
-                cond = condition_line(lines[i+j], triggers[j])
-                j+=1
-            if cond:
-                offset = 4
-            if not cond:
+        if not name_has_changed:
+            if line.lstrip()[:6] == '<name>':
+                output.append('<name>'+new_filename+'</name>')
+                name_has_changed = True
+            else:
                 output.append(line)
+        else:
+            if offset:
+                offset -=1
+            else:
+                line = lines[i].lstrip()
+                j = 0
+                cond = True
+                while j<5 and cond:
+                    cond = condition_line(lines[i+j], triggers[j])
+                    j+=1
+                if cond:
+                    offset = 4
+                if not cond:
+                    output.append(line)
 
     output = '\n'.join(output)
     with open(output_filename, 'w') as f:
