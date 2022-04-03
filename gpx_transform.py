6c3d96dac7b7c6371befe2c5a8eb01bc3203c8df
Paul Coste <paul@lescoste.net>
2022-04-03 18:33:35 +0200

    Organize code

--- gpx_transform.py
+++ gpx_transform.py
@@ -21,20 +21,44 @@ def get_image_directory_and_name(path):
     else :
         return path[:last_slash_index],path[last_slash_index+1:last_dot_index]
 
-def main():
-    input_filename = sys.argv[1]
-    new_filename = sys.argv[2]
-    dir,name = get_image_directory_and_name(input_filename)
-    result_path = dir+'/output-'+name+'.gpx'
-    output_filename = result_path
+def file_transform(file): #adds \n between all <smthg>, apply .split('\n'), removes empty lines, replaces name attributes on one line
+    oneline = ''
+    for char in file:
+        if char == '<':
+            oneline += '\n'+char
+        elif  char == '>':
+            oneline += char+'\n'
+        else:
+            oneline += char
+    lines = [line for line in oneline.split('\n') if line != '']
+    lines_with_name_fixed = []
+    n = len(lines)
+    i = 0
+    while i<n:
+        line = lines[i]
+        if line == '<name>':
+            for k in range(2):
+                i += 1
+                line += lines[i]
+        lines_with_name_fixed.append(line)
+        i += 1
+    lines_with_coos_fixed = []
+    n = len(lines_with_name_fixed)
+    i = 0
+    while i<n:
+        line = lines_with_name_fixed[i]
+        if line[:7] == '<trkpt ':
+            for k in range(3):
+                i += 1
+                line += lines_with_name_fixed[i]
+        lines_with_coos_fixed.append(line)
+        i += 1
+    return lines_with_coos_fixed
 
+def remove_seg(lines, new_filename):
     name_has_changed = False
-
-    with open(input_filename, "r") as f:
-        lines = f.read().split("\n")
     output = []
     offset = 0
-
     for i,line in enumerate(lines):
         if not name_has_changed:
             if line.lstrip()[:6] == '<name>':
@@ -56,8 +80,18 @@ def main():
                     offset = 4
                 if not cond:
                     output.append(line)
+    return '\n'.join(output)
 
-    output = '\n'.join(output)
+def main():
+    input_filename = sys.argv[1]
+    new_filename = sys.argv[2]
+    dir,name = get_image_directory_and_name(input_filename)
+    output_filename = dir+'/output-'+name+'.gpx'
+
+    with open(input_filename, "r") as f:
+        lines = f.read()
+    lines = file_transform(lines) 
+    output = remove_seg(lines, new_filename)
     with open(output_filename, 'w') as f:
         f.write(output)
 
